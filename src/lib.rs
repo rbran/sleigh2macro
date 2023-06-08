b962b9d22d4927417bbda9b8f7c3c759bb50ff4a
Rubens Brandao <git@rubens.io>
2023-06-08 10:44:33 -0300

    Bump to 0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -29,6 +29,6 @@ pub fn generate_disasembler(
             std::env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| "".into());
         Cow::Owned(Path::new(&build_path).join(&filename))
     };
-    let sleigh = sleigh2rust::parse_disassembler(&filename).unwrap();
+    let sleigh = sleigh2rust::generate_disassembler(&filename).unwrap();
     sleigh.into_token_stream().into()
 }
